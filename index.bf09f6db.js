cb6e8b35bfc89d4b36b3bd3a65c1644b81009ba2
GitHub Action <action@github.com>
2023-03-06 15:03:38 +0000

    deploy

---
+++ index.bf09f6db.js
@@ -0,0 +1,2 @@
+function myFunction(){let d=document.getElementById("about"),e=document.getElementById("plus"),s=document.getElementById("minus");d.classList.contains("hidden")?(d.classList.remove("hidden"),s.classList.remove("hidden"),e.classList.add("hidden")):(d.classList.add("hidden"),e.classList.remove("hidden"),s.classList.add("hidden"))}
+//# sourceMappingURL=index.bf09f6db.js.map
